d60f406993f51f6f18668267322f00f7adbebc21
futurella <futurella123@gmail.com>
2023-04-07 19:32:48 +0300

    commit init

---
+++ src/main/java/com/example/SlonApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlonApplication.class, args);
+    }
+
+}
